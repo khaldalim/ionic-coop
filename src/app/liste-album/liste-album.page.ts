f197cf2762b1a0e36ece33842d3cc4d981239fbe
jamalTa <taoufiqui.j@hotmail.fr>
2021-02-19 16:27:17 +0100

    creation album

---
+++ src/app/liste-album/liste-album.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-album',
+  templateUrl: './liste-album.page.html',
+  styleUrls: ['./liste-album.page.scss'],
+})
+export class ListeAlbumPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
